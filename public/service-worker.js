bf0e8b64327167bcf01c18dbc7c06497fd1be495
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-09-12 23:22:14 -0600

    updated service worker to allow application to be downloadable

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,3 +21,49 @@ self.addEventListener("install", (event) => {
 
     self.skipWaiting();
 });
+
+// activate
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
